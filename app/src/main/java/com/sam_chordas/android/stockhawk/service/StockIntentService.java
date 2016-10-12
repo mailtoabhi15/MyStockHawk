40b10fc7a2806140db22f6bd442d8dd623c389db
mailtoabhi15 <mailtoabhi15@users.noreply.github.com>
2016-10-12 20:22:34 +0530

    impl: inprogress work for Widget implementation

--- app/src/main/java/com/sam_chordas/android/stockhawk/service/StockIntentService.java
+++ app/src/main/java/com/sam_chordas/android/stockhawk/service/StockIntentService.java
@@ -1,15 +1,27 @@
 package com.sam_chordas.android.stockhawk.service;
 
 import android.app.IntentService;
+import android.app.PendingIntent;
+import android.appwidget.AppWidgetManager;
+import android.content.ComponentName;
+import android.content.Context;
 import android.content.Intent;
+import android.database.Cursor;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Looper;
 import android.util.Log;
+import android.widget.RemoteViews;
 import android.widget.Toast;
 
 import com.google.android.gms.gcm.TaskParams;
 import com.sam_chordas.android.stockhawk.R;
+import com.sam_chordas.android.stockhawk.data.QuoteColumns;
+import com.sam_chordas.android.stockhawk.data.QuoteProvider;
+import com.sam_chordas.android.stockhawk.ui.MyStocksActivity;
+import com.sam_chordas.android.stockhawk.ui.StockHawkAppWidget;
+
+import static android.R.id.input;
 
 /**
  * Created by sam_chordas on 10/1/15.
@@ -24,7 +36,38 @@ public class StockIntentService extends IntentService {
     super(name);
   }
 
+  void updateAppWidget(Context context, AppWidgetManager appWidgetManager,
+                              int appWidgetId) {
+
+    Cursor c =context.getContentResolver().query(QuoteProvider.Quotes.CONTENT_URI,
+            new String[] { "Distinct " + QuoteColumns.SYMBOL }, null,
+            null, null);
+    if (c != null && c.getCount() != 0) {
+
+    }
+    CharSequence widgetText = c.getString(c.getColumnIndex("symbol"));
+    // Construct the RemoteViews object
+    RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.stock_hawk_app_widget);
+    views.setTextViewText(R.id.appwidget_text, widgetText);
+
+    Intent intent =new Intent(context, MyStocksActivity.class);
+    PendingIntent pendingIntent = PendingIntent.getActivity(context,0,intent,0);
+    views.setOnClickPendingIntent(R.id.appwidget_text,pendingIntent);
+
+    // Instruct the widget manager to update the widget
+    appWidgetManager.updateAppWidget(appWidgetId, views);
+  }
+
+
   @Override protected void onHandleIntent(final Intent intent) {
+
+    AppWidgetManager appWidgetManager = AppWidgetManager.getInstance(this);
+    int[] appWidgetIds = appWidgetManager.getAppWidgetIds(new ComponentName(this, StockHawkAppWidget.class));
+
+    for (int appWidgetId : appWidgetIds) {
+      updateAppWidget(this, appWidgetManager, appWidgetId);
+    }
+
     Log.d(StockIntentService.class.getSimpleName(), "Stock Intent Service");
     StockTaskService stockTaskService = new StockTaskService(this);
     Bundle args = new Bundle();
@@ -37,7 +80,7 @@ public class StockIntentService extends IntentService {
       stockTaskService.onRunTask(new TaskParams(intent.getStringExtra("tag"), args));
     }
     catch (Exception e){
-      //refernce from: http://stackoverflow.com/a/28318124
+      //DIXIT-IMP:refernce from: http://stackoverflow.com/a/28318124
       Handler handler=new Handler(Looper.getMainLooper());
       handler.post(new Runnable() {
         @Override
@@ -51,4 +94,6 @@ public class StockIntentService extends IntentService {
     }
 
   }
+
+
 }
